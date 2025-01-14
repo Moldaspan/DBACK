852e3a81eb0c7f2f7bdf1be25e6502d25ca38a9c
Moldaspan <ersultanmoldaspanov@mail.ru>
2025-01-14 23:38:14 +0500

    upd

--- demeu/settings.py
+++ demeu/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,6 +63,7 @@ EMAIL_HOST_USER = 'eramoldaspan@gmail.com'
 EMAIL_HOST_PASSWORD = 'rite pdfb jtiv lrdc'
 DEFAULT_FROM_EMAIL = 'ermoldaspan@gmail.com'
 SITE_URL = 'http://localhost:8000/'
+
 
 
 TEMPLATES = [
